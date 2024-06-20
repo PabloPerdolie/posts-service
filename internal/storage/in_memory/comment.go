dc016bdec216eebc20ef54d16bce5bd73c72234b
Pablo <parusov.pashka@ya.ru>
2024-06-20 17:38:08 +0300

    add dependencies and modeified repos

--- internal/storage/in_memory/comment.go
+++ internal/storage/in_memory/comment.go
@@ -1,32 +1,43 @@
 package in_memory
 
-import "graphql-comments/internal/models"
+import (
+	"graphql-comments/internal/models"
+	"graphql-comments/internal/storage"
+	"sync"
+)
 
-func (st *InMemoryStore) AddComment(comment *models.Comment) {
-	st.commentMutex.Lock()
-	defer st.commentMutex.Unlock()
-	st.comments[comment.ID] = comment
-	if comment.ParentID != nil {
-		parent, exists := st.comments[*comment.ParentID]
-		if exists {
-			parent.Children = append(parent.Children, comment)
-		}
-	} else {
-		post, exists := st.posts[comment.PostID]
-		if exists {
-			post.Comments = append(post.Comments, comment)
-		}
+type commentRepository struct {
+	comments     map[string]*models.Comment
+	commentMutex sync.RWMutex
+}
+
+func NewCommentRepository() storage.CommentRepository {
+	return &commentRepository{
+		comments: make(map[string]*models.Comment, 10),
 	}
 }
 
-func (st *InMemoryStore) GetComments(postID string) []*models.Comment {
-	st.commentMutex.RLock()
-	defer st.commentMutex.RUnlock()
-	comments := make([]*models.Comment, 0)
-	for _, comment := range st.comments {
+func (ct *commentRepository) AddComment(comment *models.Comment) (error, models.Comment) {
+	ct.commentMutex.Lock()
+	defer ct.commentMutex.Unlock()
+	ct.comments[comment.ID] = comment
+	if comment.ParentID != nil {
+		parent, exists := ct.comments[*comment.ParentID]
+		if exists {
+			parent.Children = append(parent.Children, comment)
+		}
+	}
+	return nil, *comment
+}
+
+func (ct *commentRepository) GetComments(postID string) (error, []*models.Comment) {
+	ct.commentMutex.RLock()
+	defer ct.commentMutex.RUnlock()
+	comments := make([]*models.Comment, 255)
+	for _, comment := range ct.comments {
 		if comment.PostID == postID && comment.ParentID == nil {
 			comments = append(comments, comment)
 		}
 	}
-	return comments
+	return nil, comments
 }
