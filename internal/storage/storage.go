e407ebc2234d11f0057d3d4a1927f578f7af2584
Pablo <parusov.pashka@ya.ru>
2024-06-20 17:39:56 +0300

    renamed contracts

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,13 +2,13 @@ package storage
 
 import "graphql-comments/internal/models"
 
-type PostStorage interface {
+type PostRepository interface {
 	AddPost(post *models.Post) (error, models.Post)
 	GetPosts() (error, []*models.Post)
 	GetPost(id string) (error, *models.Post)
 }
 
-type CommentStorage interface {
+type CommentRepository interface {
 	AddComment(comment *models.Comment) (error, models.Comment)
 	GetComments(postID string) (error, []*models.Comment)
 }
