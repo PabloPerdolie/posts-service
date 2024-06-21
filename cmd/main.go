e02941af03a6749c1179317d79721569bb171780
Pablo <parusov.pashka@ya.ru>
2024-06-21 15:07:49 +0300

    final vers of main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"context"
+	"graphql-comments/internal/app"
+	"log"
+)
+
+func main() {
+	a, err := app.NewApp(context.Background())
+	if err != nil {
+		log.Fatalf("failed to init app: %s", err.Error())
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatalf("failed to run app: %s", err.Error())
+	}
+}
