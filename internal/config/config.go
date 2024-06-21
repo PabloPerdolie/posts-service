4640619bf6c535ce9e6fa0de8032bf52f60c99e7
Pablo <parusov.pashka@ya.ru>
2024-06-21 20:50:28 +0300

    config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
+var CONFIG Config
+
+type Config struct {
+	Env    string `yaml:"env"`
+	Server struct {
+		URL string `yaml:"url" env-default:"0.0.0.0:3001"`
+	} `yaml:"server"`
+	UseInMemory bool `yaml:"useInMemory"`
+	DB          struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"db"`
+}
+
+func InitConfig() error {
+	if err := cleanenv.ReadConfig("config/config.yml", &CONFIG); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	log.Println("Successfully initialized config")
+	return nil
+}
