de4a9e84606300a45155704b587d54441a95ead6
Pablo <parusov.pashka@ya.ru>
2024-06-21 15:04:13 +0300

    custom err out of length

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrPostNotFound     = errors.New("post not found")
 	ErrCommentsNotFound = errors.New("comments not found")
+	ErrOutOfLength      = errors.New("content length exceeds 2000 characters")
 )
